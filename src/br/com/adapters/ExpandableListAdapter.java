c7d12925a0e890ac5226b65c1a52bf5649f09b12
daniel.snipher@gmail.com <daniel.snipher@gmail.com@5c919911-f298-ce1c-c3f7-04edb0dae848>
2013-05-11 18:56:08 +0000

    Linkar historico de transações com banco de dados

--- src/br/com/adapters/ExpandableListAdapter.java
+++ src/br/com/adapters/ExpandableListAdapter.java
@@ -3,6 +3,9 @@ package br.com.adapters;
 import java.util.List;
 import java.util.Map;
 
+import br.com.database.DatabaseHandler;
+import br.com.entities.Transacao;
+import br.com.saldobilhete.HistoricoListActivity;
 import br.com.saldobilhete.R;
  
 import android.app.Activity;
@@ -21,18 +24,18 @@ import android.widget.TextView;
 public class ExpandableListAdapter extends BaseExpandableListAdapter {
  
     private Activity context;
-    private Map<String, List<String>> laptopCollections;
-    private List<String> laptops;
+    private Map<String, List<Transacao>> transactionsCollections;
+    private List<String> dates;
  
-    public ExpandableListAdapter(Activity context, List<String> laptops,
-            Map<String, List<String>> laptopCollections) {
+    public ExpandableListAdapter(Activity context, List<String> dates,
+            Map<String, List<Transacao>> transactionsCollections) {
         this.context = context;
-        this.laptopCollections = laptopCollections;
-        this.laptops = laptops;
+        this.transactionsCollections = transactionsCollections;
+        this.dates = dates;
     }
  
     public Object getChild(int groupPosition, int childPosition) {
-        return laptopCollections.get(laptops.get(groupPosition)).get(childPosition);
+        return transactionsCollections.get(dates.get(groupPosition)).get(childPosition);
     }
  
     public long getChildId(int groupPosition, int childPosition) {
@@ -41,7 +44,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
  
     public View getChildView(final int groupPosition, final int childPosition,
             boolean isLastChild, View convertView, ViewGroup parent) {
-        final String laptop = (String) getChild(groupPosition, childPosition);
+        final Transacao transaction = (Transacao) getChild(groupPosition, childPosition);
         LayoutInflater inflater = context.getLayoutInflater();
  
         if (convertView == null) {
@@ -60,8 +63,10 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
                 builder.setPositiveButton("Sim",
                         new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                List<String> child =
-                                    laptopCollections.get(laptops.get(groupPosition));
+                                List<Transacao> child =
+                                		transactionsCollections.get(dates.get(groupPosition));
+                                DatabaseHandler db = new DatabaseHandler(context);
+                                db.deleteTransaction(child.get(childPosition));
                                 child.remove(childPosition);
                                 notifyDataSetChanged();
                             }
@@ -77,20 +82,20 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
             }
         });
  
-        item.setText(laptop);
+        item.setText(transaction.toListString());
         return convertView;
     }
  
     public int getChildrenCount(int groupPosition) {
-        return laptopCollections.get(laptops.get(groupPosition)).size();
+        return transactionsCollections.get(dates.get(groupPosition)).size();
     }
  
     public Object getGroup(int groupPosition) {
-        return laptops.get(groupPosition);
+        return dates.get(groupPosition);
     }
  
     public int getGroupCount() {
-        return laptops.size();
+        return dates.size();
     }
  
     public long getGroupId(int groupPosition) {
@@ -99,7 +104,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
  
     public View getGroupView(int groupPosition, boolean isExpanded,
             View convertView, ViewGroup parent) {
-        String laptopName = (String) getGroup(groupPosition);
+        String transactionName = (String) getGroup(groupPosition);
         if (convertView == null) {
             LayoutInflater infalInflater = (LayoutInflater) context
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -108,7 +113,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         }
         TextView item = (TextView) convertView.findViewById(R.id.data);
         item.setTypeface(null, Typeface.BOLD);
-        item.setText(laptopName);
+        item.setText(transactionName);
         return convertView;
     }
  
